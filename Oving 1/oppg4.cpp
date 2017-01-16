796bc4d3ace017e8a5095e4d39867dba5f97dbf1
tobulf <tobulf@gmail.com>
2017-01-16 21:10:46 +0100

    Øving 1 i C++
    C++ versionen med C++11 som shell

---
+++ Oving 1/oppg4.cpp
@@ -0,0 +1,30 @@
+
+
+#include<iostream>
+#include <thread>
+
+
+using namespace std;
+
+static int i = 0;
+
+void threadfunction_1() {
+	for (int n = 0; n < 1000000; n++) {
+		i++;
+	}
+}
+
+void threadfunction_2() {
+	for (int n = 0; n < 1000000; n++){
+		i--;
+	}
+}
+
+int main() {
+	thread thread_1(threadfunction_1);
+	thread thread_2(threadfunction_2);
+	thread_1.join();
+	thread_2.join();
+	cout << i << endl;
+	return 0;
+}
