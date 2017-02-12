974b757f9213257296719786dd7be718977f5689
tobulf <tobulf@gmail.com>
2017-02-12 03:14:42 +0100

    Add files via upload

--- Heisprosjekt/UDP/UDP_test_server.py
+++ Heisprosjekt/UDP/UDP_test_server.py
@@ -1,12 +1,12 @@
-from UDP_driver import UdpServer
+from UDP_driver import UDP_CLIENT
 import time
 adress=("129.241.187.255",20010)
 serverAdress=("120.23.40.5",230)
 
-server=UdpServer(adress,serverAdress)
+server=UDP_CLIENT(adress,serverAdress)
 
 while True:
-    server.Heartbeat()
+    server.heartbeat()
     time.sleep(1)
 
 
