178684710c2ce04382380be056c4005df2fedc3e
tobulf <tobulf@gmail.com>
2017-03-13 01:43:37 +0100

    Fixed issues

--- Heisprosjekt/Final/Order.py
+++ Heisprosjekt/Final/Order.py
@@ -11,36 +11,36 @@ class Order(object):
 
     def ExternalOrderServed(self, Floor, Direction, State):
         # Assert if there was an order served at that floor, returns it and deletes it:
-        if Direction == Motor_direction.DIRN_UP and self.orderUp[Floor]:
+        if Direction == MotorDirection.DirnUp and self.orderUp[Floor]:
             self.DeleteOrder(Floor, Direction)
             return [Floor, Direction]
 
-        elif Direction == Motor_direction.DIRN_DOWN and self.orderDown[Floor]:
+        elif Direction == MotorDirection.DirnDown and self.orderDown[Floor]:
             self.DeleteOrder(Floor, Direction)
             return [Floor, Direction]
 
 
-        elif Direction == Motor_direction.DIRN_STOP and (self.orderDown[Floor] or self.orderUp[Floor]):
-            self.DeleteOrder(Floor, Motor_direction.DIRN_DOWN)
-            self.DeleteOrder(Floor, Motor_direction.DIRN_UP)
+        elif Direction == MotorDirection.DirnStop and (self.orderDown[Floor] or self.orderUp[Floor]):
+            self.DeleteOrder(Floor, MotorDirection.DirnDown)
+            self.DeleteOrder(Floor, MotorDirection.DirnUp)
             return [Floor, Direction]
 
         # Special if the elevator is on the top or bottom:
-        elif Direction == Motor_direction.DIRN_DOWN and Floor == 0 and self.orderUp[Floor]:
-            self.DeleteOrder(Floor, Motor_direction.DIRN_UP)
-            return [Floor, Motor_direction.DIRN_UP]
+        elif Direction == MotorDirection.DirnDown and Floor == 0 and self.orderUp[Floor]:
+            self.DeleteOrder(Floor, MotorDirection.DirnUp)
+            return [Floor, MotorDirection.DirnUp]
 
-        elif Direction == Motor_direction.DIRN_UP and Floor == (self.floors-1) and self.orderDown[Floor]:
-            self.DeleteOrder(Floor, Motor_direction.DIRN_DOWN)
-            return [Floor, Motor_direction.DIRN_DOWN]
+        elif Direction == MotorDirection.DirnUp and Floor == (self.floors-1) and self.orderDown[Floor]:
+            self.DeleteOrder(Floor, MotorDirection.DirnDown)
+            return [Floor, MotorDirection.DirnDown]
 
-        elif State == Elevator_state.IDLE:
+        elif State == ElevatorState.Idle:
             if self.orderDown[Floor]:
-                self.DeleteOrder(Floor, Motor_direction.DIRN_DOWN)
-                return [Floor, Motor_direction.DIRN_DOWN]
+                self.DeleteOrder(Floor, MotorDirection.DirnDown)
+                return [Floor, MotorDirection.DirnDown]
             elif self.orderUp[Floor]:
-                self.DeleteOrder(Floor, Motor_direction.DIRN_UP)
-                return [Floor, Motor_direction.DIRN_UP]
+                self.DeleteOrder(Floor, MotorDirection.DirnUp)
+                return [Floor, MotorDirection.DirnUp]
 
         else:
             return None
@@ -48,9 +48,9 @@ class Order(object):
 
 
     def DeleteOrder(self, Floor, Direction):
-        if Direction == Motor_direction.DIRN_UP:
+        if Direction == MotorDirection.DirnUp:
             self.orderUp[Floor] = False
-        elif Direction == Motor_direction.DIRN_DOWN:
+        elif Direction == MotorDirection.DirnDown:
             self.orderDown[Floor] = False
 
     def AppendOrder(self, Order, ClientObject):
