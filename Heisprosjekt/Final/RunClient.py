a95c6829689ec99067dd4d27864b0787fb7af511
tobulf <tobulf@gmail.com>
2017-03-15 04:35:48 +0100

    Some fixup

---
+++ Heisprosjekt/Final/RunClient.py
@@ -0,0 +1,213 @@
+from threading import Lock, Thread
+from LightCtrl import *
+from TypeClasses import *
+from ButtonsPressed import *
+import subprocess as Respawn
+from Order import Order
+from ClientObject import Client
+from Elevator import Elevator
+from HTTPClient import HttpClient
+from UDPServer import UdpServer
+from Timer import Timer
+import netifaces as FindIP
+
+
+# Finding IP
+FindIP.ifaddresses('eth0')
+Address = FindIP.ifaddresses('eth0')[2][0]['addr']
+Port = 20011
+
+# Declaring vital objects:
+elevator = Elevator()
+mutex = Lock()
+Httpclient = HttpClient("", Port)
+ClientObject = Client()
+ClientUDP = UdpServer(Port)
+RequestTimer = Timer()
+
+
+def ElevatorThread():
+    elevator.Serve()
+
+def UDPThread():
+    global Httpclient
+    while True:
+        ClientUDP.Listen()
+        # If the Client is disconnected and has no HttpConnection:
+        if ClientUDP.connected and not Httpclient.connected:
+            # Connect the Httpclient
+            mutex.acquire()
+            Httpclient = HttpClient(ClientUDP.ServerAddress, Port)
+            Httpclient.connected = True
+            mutex.release()
+        # To reset the Client if it gets disconnected:
+        elif not ClientUDP.connected and Httpclient.connected:
+            mutex.acquire()
+            Httpclient.connected = False
+            mutex.release()
+
+
+def ButtonThread(RefreshRate = 0.1, PrintRate = 1):
+    # Declare an orderobject to keep control of orders:
+    OrderObject = Order()
+    # Declaring Buttonobject, to poll buttons:
+    Buttons = ButtonObject()
+    printTimer = Timer()
+    printTimer.StartTimer()
+    while True:
+        # Everything starts with polling on the Buttons:
+        pressed = Buttons.ButtonPressed()
+        # Basicaly a statemachine for the Client:
+        if pressed and ClientUDP.connected:
+            RequestTimer.StartTimer()
+            floor, button, externalorder = pressed
+            if externalorder:
+                # Declare a ClientObject which contain all necessary information to the server:
+                ClientObject = Client(Address=Address, Order=[floor, button], Direction=elevator.direction,
+                                      Position=elevator.currentfloor, InternalOrders=elevator.InternalQueue,
+                                      OrderCompleted=OrderObject.ExternalOrderServed(elevator.currentfloor, elevator.direction, elevator.currentstate),
+                                      CurrentState=elevator.currentstate, OrderUp=OrderObject.orderUp, OrderDown=OrderObject.orderDown)
+                # Using mutex before making a request, to prevent concurrency if the order never gets trough:
+                ClientObject = Httpclient.PostRequest("GotOrder", ClientObject.toJson())
+                if ClientObject:
+                    # Add Potential order:
+                    OrderObject.AppendOrder(ClientObject)
+                    # Update external queues:
+                    mutex.acquire()
+                    elevator.ExternalQueueUp   = OrderObject.orderUp
+                    elevator.ExternalQueueDown = OrderObject.orderDown
+                    mutex.release()
+                    # Set all the ligths:
+                    for i in range(elevator.floors):
+                        if ClientObject.lightsDown[i]:
+                            SetLigth(i, LampType.CallDown)
+                        if ClientObject.lightsUp[i]:
+                            SetLigth(i, LampType.CallUp)
+                        if not ClientObject.lightsDown[i]:
+                            KillLight(i, LampType.CallDown)
+                        if not ClientObject.lightsUp[i]:
+                            KillLight(i, LampType.CallUp)
+                elif not ClientObject:
+                    # If the Client has issues submitting the order to the server it just takes the order itself:
+                    if button == ButtonType.CallDown:
+                        elevator.ExternalQueueDown[floor] = True
+                        SetLigth(floor, ButtonType.CallDown)
+                    if button == ButtonType.CallUp:
+                        elevator.ExternalQueueUp[floor] = True
+                        SetLigth(floor, ButtonType.CallUp)
+            else:
+                # if not an externalOrder, just add the order to internal queue:
+                mutex.acquire()
+                elevator.InternalQueue[floor] = True
+                mutex.release()
+                # Set the lights:
+                SetLigth(floor, LampType.Command)
+#
+        elif not pressed and ClientUDP.connected and (RequestTimer.GetCurrentTime() > RefreshRate or not RequestTimer.started):
+            # Reset the Request-timer:
+            RequestTimer.StartTimer()
+            # Check if any orders ar served
+            OrderCompleted = OrderObject.ExternalOrderServed(elevator.currentfloor, elevator.direction, elevator.currentstate)
+            # Declare a ClientObject which contain all necessary information to the server:
+            ClientObject = Client(Address=Address, Direction = elevator.direction, Position=elevator.currentfloor,
+                                  InternalOrders=elevator.InternalQueue, OrderCompleted=OrderCompleted,
+                                  CurrentState=elevator.currentstate, OrderUp=OrderObject.orderUp,
+                                  OrderDown=OrderObject.orderDown)
+            ClientObject = Httpclient.PostRequest("GetUpdate", ClientObject.toJson())
+            # Check if there are a Clientobject:
+            if ClientObject:
+                # Add an potential order to the orderlist:
+                OrderObject.AppendOrder(ClientObject)
+                # Update external queues:
+                mutex.acquire()
+                elevator.ExternalQueueUp = OrderObject.orderUp
+                elevator.ExternalQueueDown = OrderObject.orderDown
+                mutex.release()
+                # Reset the lights for the external orders:
+                for i in range(elevator.floors):
+                    if ClientObject.lightsDown[i]:
+                        SetLigth(i, LampType.CallDown)
+                    if ClientObject.lightsUp[i]:
+                        SetLigth(i, LampType.CallUp)
+                    if not ClientObject.lightsDown[i]:
+                        KillLight(i, LampType.CallDown)
+                    if not ClientObject.lightsUp[i]:
+                        KillLight(i, LampType.CallUp)
+
+        # If the Client is not connected to a server, it works as a ordinary elevator:
+
+        elif pressed and not ClientUDP.connected:
+            floor, button, externalorder = pressed
+            # Mutex to prevent concurrency:
+            mutex.acquire()
+            if button == ButtonType.CallDown:
+                elevator.ExternalQueueDown[floor] = True
+                OrderObject.orderDown = elevator.ExternalQueueDown
+                SetLigth(floor, ButtonType.CallDown)
+            if button == ButtonType.CallUp:
+                elevator.ExternalQueueUp[floor] = True
+                OrderObject.orderUp = elevator.ExternalQueueUp
+                SetLigth(floor, ButtonType.CallUp)
+            if button == ButtonType.Command:
+                elevator.InternalQueue[floor] = True
+                SetLigth(floor, ButtonType.Command)
+            mutex.release()
+            
+        elif not ClientUDP.connected:
+            RequestTimer.StopTimer()
+            # Turn of all lights that are not in the orderList:
+            for i in range(elevator.floors):
+                if not elevator.ExternalQueueUp[i]:
+                    OrderObject.orderUp[i] = False
+                    KillLight(i, ButtonType.CallUp)
+                if not elevator.ExternalQueueDown[i]:
+                    OrderObject.orderDown[i] = False
+                    KillLight(i, ButtonType.CallDown)
+        # Kills the Network if the elevator has gone into errormode:
+        if elevator.currentstate == ElevatorState.Error:
+            # Send the latest state:
+            ClientObject = Client(Address=Address, Direction=elevator.direction, Position=elevator.currentfloor,
+                                  InternalOrders=elevator.InternalQueue, OrderCompleted=OrderCompleted,
+                                  CurrentState=elevator.currentstate, OrderUp=OrderObject.orderUp,
+                                  OrderDown=OrderObject.orderDown)
+            Httpclient.PostRequest("GetUpdate", ClientObject.toJson())
+            Respawn.Popen(["gnome-terminal", "-x", "sh", "-c", "python RunClient.py"])
+            break
+
+        # Small print function to keep track of what state the elevator is in:
+        if printTimer.GetCurrentTime() > PrintRate:
+            printTimer.StartTimer()
+            if ClientUDP.connected:
+                print "___________________________________________"
+                print "Currently being served by: ", ClientUDP.ServerAddress
+            else:
+                print "___________________________________________"
+                print "Currently disconnected..."
+
+
+
+def MainThread():
+    # Declaring all threads;
+    Thread1 = Thread(target = ButtonThread, args = (),)
+    Thread2 = Thread(target = ElevatorThread, args = (),)
+    Thread3 = Thread(target = UDPThread, args = (),)
+    # Making all threads Sub-Threads
+    Thread1.daemon = True
+    Thread2.daemon = True
+    Thread3.daemon = True
+    # Starting threads
+    Thread1.start()
+    Thread2.start()
+    Thread3.start()
+    # Keep the main thread alive until Thread1 is finished. This only happens if an error occurs.
+    while Thread1.isAlive():
+        pass
+
+
+MainThread()
+
+
+
+
+
+
---
+++ Heisprosjekt/Final/RunServer.py
@@ -0,0 +1,144 @@
+from BaseHTTPServer import BaseHTTPRequestHandler
+from json import dumps
+from threading import Thread,Lock
+from HTTPServer import HttpServer
+from HTTPClient import HttpClient
+from ClientObject import Client
+from QueueMaster import QueueMaster
+from UDPClient  import UdpClient
+import netifaces as FindIP
+from Timer import Timer
+import subprocess as Respawn
+
+# Finding the IP:
+FindIP.ifaddresses('eth0')
+IP = FindIP.ifaddresses('eth0')[2][0]['addr']
+Bcast = FindIP.ifaddresses('eth0')[2][0]['broadcast']
+Port  = 20011
+
+
+# Declare the queuemasterobject for this server
+Queuemaster = QueueMaster()
+# Heartbeat object:
+heartbeat = UdpClient(Bcast, IP, Port)
+# Declares a Client, binds it to port zero and local.
+backupclient = HttpClient("",0)
+# Mutex
+Mutex = Lock()
+# Request-Timer T keep the Dormant server from Spamming to much requests:
+RequestTimer = Timer()
+
+
+# RequestHandler which Does all the work for the server:
+class RequestHandler(BaseHTTPRequestHandler):
+    # Custom made handler class for the Networkmodule:
+    def do_POST(self):
+        global Queuemaster
+        content_len = int(self.headers.getheader('content-length', 0)) #Finding the length of the response body
+        body = self.rfile.read(content_len) #Extracting the body
+        try:
+            # Declare the Client object to receive:
+            Clientobject = Client()
+            # Answer is calculated and serialized directly
+            if self.path == "GotOrder":
+                # Load Clientobject from json
+                Clientobject.fromJson(body)
+                answer = Queuemaster.GotOrder(Clientobject).toJson()
+                self.send_response(200, answer)  # Send the proper response and status
+            elif self.path == "GetUpdate":
+                # Load Clientobject from json
+                Clientobject.fromJson(body)
+                answer = Queuemaster.GetUpdate(Clientobject).toJson()
+                self.send_response(200, answer)  # Send the proper response and status
+            elif self.path == "Test":
+                self.send_response(200,dumps("hei"))
+            else:
+                answer = dumps("Path Dont exist")
+                self.send_response(404, dumps(answer)) #Send the proper response and status
+        except TypeError:
+            pass
+
+    def do_GET(self):
+        global Queuemaster
+        # Only dormant server post Get:
+        answer = Queuemaster.toJson()
+        self.send_response(200, answer)
+    # Handle the logs
+    def log_message(self, format, *args):
+        return
+
+Handler = RequestHandler
+server  = HttpServer(IP, Port, Handler)
+server.serving = True
+
+def HTTPThread(RefreshRate = 0.1):
+    global Queuemaster
+    RequestTimer.StartTimer()
+    while True:
+        if heartbeat.ServingServer:
+            RequestTimer.StopTimer()
+            try:
+                # Serve request:
+                server.ServeOnce()
+                # Check for Timeouts
+                Queuemaster.CheckTimeout()
+            except AttributeError:
+                print "Error"
+                pass
+        else:
+            if backupclient.connected and RequestTimer.GetCurrentTime() > RefreshRate:
+                # Reset the timer:
+                RequestTimer.StartTimer()
+                # Ask for backup:
+                backup = backupclient.GetRequest()
+                if backup:
+                    # If backup, replace old:
+                    print
+                    Queuemaster = backup
+
+
+def UDPThread(PrintRate = 1):
+    printTimer = Timer()
+    printTimer.StartTimer()
+    while True:
+        global backupclient
+        if heartbeat.ServingServer:
+            heartbeat.Heartbeat()
+        else:
+            heartbeat.Listen()
+            if heartbeat.ServingServer:
+                print "Now Serving!"
+                backupclient.connected = False
+
+            elif not backupclient.connected and not heartbeat.ServingServer:
+                # Bind the Client to the Ip and port of current serving server:
+                Mutex.acquire()
+                backupclient = HttpClient(heartbeat.ServerAdress, Port)
+                Mutex.release()
+                backupclient.connected = True
+            # Little print function:
+            if printTimer.GetCurrentTime() > PrintRate:
+                printTimer.StartTimer()
+                print "______________________________________________________"
+                print "Currently dormant getting backup from: ", heartbeat.ServerAdress
+
+def MainThread():
+    # 2 Threads, one for the UDP and one for HTTP
+    Thread1 = Thread(target=HTTPThread, args=(), )
+    Thread2 = Thread(target=UDPThread, args=(), )
+    Thread1.daemon = True
+    Thread2.daemon = True
+    Thread2.start()
+    Thread1.start()
+    print "Server Starting up..."
+    while Thread1.is_alive():
+        pass
+    Respawn.Popen(["gnome-terminal", "-x", "sh", "-c", "python RunServer.py"])
+
+
+MainThread()
+
+
+
+
+
